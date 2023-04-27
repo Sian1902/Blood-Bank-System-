bbd7579d000bb734913ebd593f77e6805d5a836b
OsamaEmam314 <osamakhaled31415@gmail.com>
2023-04-27 23:29:56 +0200

    345

--- bloodBankSystem/bloodBankSystem/main.cpp
+++ bloodBankSystem/bloodBankSystem/main.cpp
@@ -1,8 +1,19 @@
 #include "bloodBankSystem.h"
 #include <QtWidgets/QApplication>
+<<<<<<< Updated upstream
 #include"userClass.h"
 int main(int argc, char *argv[])
 {
+=======
+#include"UserClass.h"
+#include"BloodBankClass.h"
+int main(int argc, char *argv[])
+{
+    time_t t;
+    time(&t);
+    BloodBankClass b;
+    b.Regestration("hamed", "1234", "hamed@gmail.com", "6666", t, "male", "o", "tt", "t1");
+>>>>>>> Stashed changes
     QApplication a(argc, argv);
     bloodBankSystem w;
     w.show();
