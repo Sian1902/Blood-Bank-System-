41944e102f4aa6ff922d0a85d715166ceaa3f3df
OsamaEmam314 <osamakhaled31415@gmail.com>
2023-04-28 16:26:39 +0300

    ee

--- bloodBankSystem/bloodBankSystem/main.cpp
+++ bloodBankSystem/bloodBankSystem/main.cpp
@@ -12,7 +12,12 @@ int main(int argc, char *argv[])
     time_t t;
     time(&t);
     BloodBankClass b;
+<<<<<<< Updated upstream
     b.Regestration("hamed", "1234", "hamed@gmail.com", "6666", t, "male", "o", "tt", "t1");
+>>>>>>> Stashed changes
+=======
+    b.Regestration("osama", "3456789", "osama3@gmail.com", "32d13",t/2,"male","0+","fdd","sifddso");
+
 >>>>>>> Stashed changes
     QApplication a(argc, argv);
     bloodBankSystem w;
